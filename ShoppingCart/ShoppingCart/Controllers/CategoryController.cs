6ee0733c11c7e8f7095891742e6e8a86e58d97f4
maneesh kc <kc.maneesh@gmail.com>
2025-03-29 14:20:12 +0530

    Categoary create functionality

--- ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
+++ ShoppingCart/ShoppingCart/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingCart.Data;
+using ShoppingCart.Models;
 
 namespace ShoppingCart.Controllers
 {
@@ -12,8 +13,64 @@ namespace ShoppingCart.Controllers
         }
         public IActionResult Index()
         {
-            List<Models.Category> items =_context.Categories.ToList();
+            List<Models.Category> items = _context.Categories.ToList();
             return View(items);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category cat)
+        {
+            if(cat.DisplayOrder<=0)
+            {
+                ModelState.AddModelError("", "Display order should be Greater than 0");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(cat);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(cat);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCategory(int id)
+        {
+
+            Category? item = _context.Categories.Find(id);
+            _context.Categories.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
